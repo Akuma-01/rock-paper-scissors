f5c3fc5b045e60a361b109db0db983c81ea7a037
Anurag Shah <anuragshah2024@gmail.com>
2024-05-30 00:09:51 +0530

    Just a safe point

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice()    
+{
+    let compChoice = Math.round(Math.random() * 100);
+
+    if (compChoice % 3 === 0) 
+    {
+        return "rock";
+    }
+    else if (compChoice % 2 === 0)
+    {
+        return "paper";
+    }
+    else 
+    {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() 
+{
+    let inp;
+    while (inp != "scissors" || inp != "rock" || inp != "paper")
+    {
+        inp = (prompt("What do you want to choose? ")).toLowerCase();
+    }
+    
+    return inp;
+}
+
+function playRound(humanChoice, computerChoice) 
+{
+    
+}
